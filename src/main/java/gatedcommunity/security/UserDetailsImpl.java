c5eaf6052f83e38800267f74e02d32ce67513232
Oleksandr0130 <olek1995sta@gmail.com>
2024-09-10 17:43:45 +0200

    create tables

--- src/main/java/gatedcommunity/security/UserDetailsImpl.java
+++ src/main/java/gatedcommunity/security/UserDetailsImpl.java
@@ -1,14 +1,15 @@
 package gatedcommunity.security;
 
+import gatedcommunity.model.dto.UserRegisterDto;
 import gatedcommunity.repository.UserRepository;
+import gatedcommunity.service.interfaces.UserService;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 
 @Service
-public class UserDetailsImpl implements UserDetailsService {
+public class UserDetailsImpl implements UserService {
 
     private final UserRepository userRepository;
 
@@ -16,9 +17,20 @@ public class UserDetailsImpl implements UserDetailsService {
         this.userRepository = userRepository;
     }
 
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findUserByUserName(username).orElseThrow(
                 () -> new UsernameNotFoundException(username));
+    }
+
+    @Override
+    public void register(UserRegisterDto registerDto) {
+
+    }
+
+    @Override
+    public String confirmationMailByCode(String code) {
+        return "";
     }
 }
