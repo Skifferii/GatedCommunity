a87b77dffc32c2caf5f2a86bcfee6642cbf3e4b3
MaratIsmailow <westernpost.1@gmail.com>
2024-09-10 18:16:58 +0200

    Marat

--- src/main/java/gatedcommunity/service/interfaces/UserService.java
+++ src/main/java/gatedcommunity/service/interfaces/UserService.java
@@ -1,8 +1,12 @@
 package gatedcommunity.service.interfaces;
 
 import gatedcommunity.model.dto.UserRegisterDto;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface UserService  {
+
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
 
     void register(UserRegisterDto registerDto);
     String confirmationMailByCode(String code);
