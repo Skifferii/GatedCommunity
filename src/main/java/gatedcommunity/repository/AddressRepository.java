9abedefa1e50a2203f51d3ba684afa1d43a7e556
MaratIsmailow <westernpost.1@gmail.com>
2024-09-13 19:34:50 +0200

    Temp

--- src/main/java/gatedcommunity/repository/AddressRepository.java
+++ src/main/java/gatedcommunity/repository/AddressRepository.java
@@ -1,4 +1,13 @@
 package gatedcommunity.repository;
 
-public interface AddressRepository {
+import gatedcommunity.model.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AddressRepository extends JpaRepository<Address, Integer> {
+
+
+    List<Address> findAddressById (long id);
+   /* List<Address> findAddress(Long id);*/
 }
