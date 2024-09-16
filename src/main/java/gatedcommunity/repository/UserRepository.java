ce37669b0aecac047713e949b6cf1b55de926eff
Evgen Grushchenko <evgen.tm959@gmail.com>
2024-09-16 19:01:28 +0200

    Evgen003

--- src/main/java/gatedcommunity/repository/UserRepository.java
+++ src/main/java/gatedcommunity/repository/UserRepository.java
@@ -7,8 +7,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findUserByUserName(String username);
-    boolean existsByEmail(String email);
-    Optional<User> findByEmail(String email);
+//    Optional<User> findUserByUserName(String username);
+//    boolean existsByEmail(String email);
+//    Optional<User> findByEmail(String email);
 
 }
