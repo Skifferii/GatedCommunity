49ee3c440fdb8d011125b1f9641eae0df9e4dc4d
Andrii Dolzhenko <164943477+Skifferii@users.noreply.github.com>
2024-10-27 21:00:41 +0100

    Revert "Oleksandr"

--- src/main/java/projectfs44/gatedcommunity/security/security_config/SecurityConfig.java
+++ src/main/java/projectfs44/gatedcommunity/security/security_config/SecurityConfig.java
@@ -18,11 +18,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+
 public class SecurityConfig {
 
     private final TokenFilter tokenFilter;
-
-
     public SecurityConfig(TokenFilter tokenFilter) {
         this.tokenFilter = tokenFilter;
     }
@@ -32,13 +31,20 @@ public class SecurityConfig {
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
-
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // настройка сессий
-
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)) // настройка сессий
                 .httpBasic(AbstractHttpConfigurer::disable) // отключаем базовую аутентификацию
                 .addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
 
+                                .anyRequest().permitAll()
+//                                .requestMatchers(HttpMethod.GET, "/hello").permitAll()
+//                                .requestMatchers(HttpMethod.POST, "/auth/login", "/auth/refresh").permitAll()
+//                                .requestMatchers("/swagger-ui/**", "/v3/api-docs").permitAll()
+//                                .requestMatchers(HttpMethod.GET, "/products").permitAll() // разрешено всем
+//                                .requestMatchers(HttpMethod.GET, "/products/{id}").authenticated() //  только для аутентифицированных пользователей
+////                        .requestMatchers(HttpMethod.GET, "/products/{id}").hasAnyRole("ADMIN", "USER") //  только для пользователей с ролью USER или ADMIN
+//                                .requestMatchers(HttpMethod.POST, "/products").hasRole("ADMIN")
+//                                .anyRequest().authenticated()
 
 
 //                                .anyRequest().permitAll()
@@ -60,9 +66,7 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.PUT, "/addresses/{id}").hasRole("ADMIN")
                                 .requestMatchers(HttpMethod.GET,"/addresses").authenticated()
                                 .requestMatchers(HttpMethod.DELETE,"/addresses/{id}").hasRole("ADMIN")
-
-                                .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")//TODO СПРОСИТЬ!!!!!!!!!!!!
-
+                                .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
                                 .requestMatchers(HttpMethod.GET,"/users/{id}").authenticated()
                                 .requestMatchers(HttpMethod.GET,"/users").authenticated()
                                 .requestMatchers(HttpMethod.GET, "users/results").authenticated()
@@ -77,9 +81,7 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.DELETE,"/user-requests/{id}").hasRole("ADMIN")
                                 .requestMatchers(HttpMethod.PUT,"/user-requests/restore/{id}").hasRole("ADMIN")
                                 .requestMatchers(HttpMethod.PUT,"/user-requests/remove/{id}").hasRole("ADMIN")
-
-                                .anyRequest().authenticated()
-                );
+                                .anyRequest().authenticated());
 
 
         return http.build();
@@ -92,7 +94,6 @@ public class SecurityConfig {
     }
 
 }
-
 
 /*
 Уровни доступа!
